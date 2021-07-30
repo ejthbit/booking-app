4242e0faa474e7912dc4f2b43e32ff7ae825fedf
8bit <ejthbit@gmail.com>
2021-07-30 19:03:55 +0200

    Changed port of application

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ import allRoutes from './routes/index'
 
 const startServer = () => {
     dotenv.config()
+    const port = process.env.PORT || 5000
     const app = express()
     app.use(express.json(), errorHandler, express.urlencoded({ extended: true }), cors({ origin: process.env.ORIGIN }))
     allRoutes(app)
-    app.listen(8080, () => {
-        console.log(`🚀🌑 server is running on port 8080.`)
+    app.listen(port, () => {
+        console.log(`🚀🌑 server is running on port port.`)
     })
 }
 startServer()
