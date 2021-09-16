73fc1df5599bb5c9f4d2172f9d38dc4cb2c6ce68
8bit <ejthbit@gmail.com>
2021-09-16 16:43:52 +0200

    Added helmet middleware

--- src/server.js
+++ src/server.js
@@ -3,17 +3,20 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { errorHandler } from './middlewares/errorHandler'
 import allRoutes from './routes/index'
+import helmet from 'helmet'
 
 const startServer = () => {
     dotenv.config()
     const port = process.env.PORT || 5000
     const app = express()
     app.use(
+        helmet(),
         express.json(),
         errorHandler,
         express.urlencoded({ extended: true }),
         cors({ origin: process.env.ORIGIN.split(','), credentials: true })
     )
+    app.disable('x-powered-by')
     allRoutes(app)
     app.listen(port, () => {
         console.log(`🚀🌑 server is running on port ${port}.`)
